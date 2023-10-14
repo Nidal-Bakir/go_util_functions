48158ddeeeb044d453035edad140feb10509ee1f
Nidal Bakir <nidal.bakir.edu@gmail.com>
2023-10-15 00:10:00 +0300

    fix: import unicode/utf8

--- utilfn.go
+++ utilfn.go
@@ -1,5 +1,6 @@
 package utilfn
 
+import "unicode/utf8"
 
 // language agnostic function to trim a string str to maxLength
 // e.g 1: TrimString("abcd",2) // ab
@@ -9,7 +10,8 @@ func TrimString(str string, maxLength int) string {
 	accumulateByteSize := 0
 	byteSize := 0
 
-	// as log as we are lees the then the maxLength and we did not exceed the number of bytes in str
+	// as log as we are lees the then the maxLength
+	// and we did not exceed the number of bytes in str
 	for runeCount := 0; runeCount < maxLength && accumulateByteSize < byteLenOfStr; runeCount++ {
 		_, byteSize = utf8.DecodeRuneInString(str[accumulateByteSize:])
 		accumulateByteSize += byteSize
